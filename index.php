6c2fad821b17edb570d174eb459c58f818e4983b
KauaPrado <kauaprado@live.com>
2025-01-15 17:58:32 -0300

    Criado o repositorio

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Controllers\GameController;
+use Config\Database;
+$pdo = Database::connect();
+
+$controller = new GameController($pdo);
+$controller->index();
+
